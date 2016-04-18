f922c207ee995ff140b849e520149612547e76eb
Jelle Sjollema <djsjollema@gmail.com>
2016-04-18 17:49:30 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load",function(){
+    //opdracht SpriteSheet01
+    //zoek uit hoe je op een canvas van  800 x 450
+    //met behulp van spritesheet media/images/capguy-walk.png
+    // een animatie kan maken
+    
+	var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var sx,sy,sw,sh;
+    var counter = 0;
+    
+    var image = new Image();
+    image.src = "media/images/capguy-walk.png";
+    
+})
